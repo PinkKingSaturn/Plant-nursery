bb8efbdfdc30472bc07d05f56a421d18be8e6b53
Kingston Spotts <guitarhero0122@gmail.com>
2024-11-11 19:41:09 -0500

    debug

--- src/store.js
+++ src/store.js
@@ -1,19 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit';
 import cartReducer from './CartSlice';
-import {createStore} from 'redux';
-
-const initialState = {
-    cart: {},
-};
-
-const reducer = (state = initialState, action) => {
-switch (action.type) {
-    case 'ADD_ITEM':
-    return { ...state, cart: { ...state.cart, [action.item.name]: (state.cart[action.item.name] || 0) + 1 } };
-    default:
-    return state;
-    }
-};
-
-const store = createStore(reducer);
+ const store = configureStore({
+    reducer: {
+        cart: cartReducer,
+    },
+});
 export default store
